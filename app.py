bb7d63aead330aef6aba5816b60cd04b99f74e5d
AnkitXGandhi <agproxy21@yahoo.com>
2024-10-20 20:23:26 +0530

    fixes

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import os
 import streamlit as st
-import google-generativeai as genai
+import google.generativeai as genai
 import PyPDF2 as pdf
 from dotenv import load_dotenv
 
-os.system("pip install google-generativeai")
+os.system("pip install google.generativeai")
 
 load_dotenv()
 
